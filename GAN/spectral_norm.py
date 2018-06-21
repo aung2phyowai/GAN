945c8ef26d5ff64abf314795d577e1ad170af386
Kay Hartmann <kg.hartma@gmail.com>
2018-06-21 15:49:20 +0200

    Spectral norm cleanup

--- GAN/spectral_norm.py
+++ GAN/spectral_norm.py
@@ -1,5 +1,6 @@
 """
 Spectral Normalization from https://openreview.net/forum?id=B1QRgziT-
+Implemented for PyTorch using WeightNorm implementation https://pytorch.org/docs/stable/_modules/torch/nn/utils/weight_norm.html
 """
 from torch.nn.parameter import Parameter
 from torch.autograd import Variable
@@ -48,59 +49,12 @@ class SpectralNorm(object):
 
 
 def spectral_norm(module, name='weight', dim=0):
-    r"""Applies weight normalization to a parameter in the given module.
-
-    .. math::
-         \mathbf{w} = g \dfrac{\mathbf{v}}{\|\mathbf{v}\|}
-
-    Weight normalization is a reparameterization that decouples the magnitude
-    of a weight tensor from its direction. This replaces the parameter specified
-    by `name` (e.g. "weight") with two parameters: one specifying the magnitude
-    (e.g. "weight_g") and one specifying the direction (e.g. "weight_v").
-    Weight normalization is implemented via a hook that recomputes the weight
-    tensor from the magnitude and direction before every :meth:`~Module.forward`
-    call.
-
-    By default, with `dim=0`, the norm is computed independently per output
-    channel/plane. To compute a norm over the entire weight tensor, use
-    `dim=None`.
-
-    See https://arxiv.org/abs/1602.07868
-
-    Args:
-        module (nn.Module): containing module
-        name (str, optional): name of weight parameter
-        dim (int, optional): dimension over which to compute the norm
-
-    Returns:
-        The original module with the weight norm hook
-
-    Example::
-
-        >>> m = weight_norm(nn.Linear(20, 40), name='weight')
-        Linear (20 -> 40)
-        >>> m.weight_g.size()
-        torch.Size([40, 1])
-        >>> m.weight_v.size()
-        torch.Size([40, 20])
-
-    """
     SpectralNorm.apply(module, name)
     return module
 
 
 
 def remove_spectral_norm(module, name='weight'):
-    r"""Removes the weight normalization reparameterization from a module.
-
-    Args:
-        module (nn.Module): containing module
-        name (str, optional): name of weight parameter
-
-    Example:
-        >>> m = weight_norm(nn.Linear(20, 40))
-        >>> remove_weight_norm(m)
-    """
     for k, hook in module._forward_pre_hooks.items():
         if isinstance(hook, SpectralNorm) and hook.name == name:
             hook.remove(module)
